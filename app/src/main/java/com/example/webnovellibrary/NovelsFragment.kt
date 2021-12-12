7b8d7d341d7a9da99c6b9592ce9f2d87576b0fcb
Ahmad Hossain <ahmad.awaab@gmail.com>
2021-12-12 00:43:38 -0500

    novels can be added manually to folders

--- app/src/main/java/com/example/webnovellibrary/NovelsFragment.kt
+++ app/src/main/java/com/example/webnovellibrary/NovelsFragment.kt
@@ -1,16 +1,21 @@
 package com.example.webnovellibrary
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
 import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.textfield.TextInputEditText
 
 class NovelsFragment : Fragment() {
+
+    private val TAG = "NovelsFragment"
+    lateinit var webNovelsList: MutableList<WebNovel>
+    lateinit var novelsAdapter: NovelsAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_novels, container, false)
@@ -19,16 +24,15 @@ class NovelsFragment : Fragment() {
 
 //        private val args by navArgs<NovelsFragmentArgs>()     retrieve using args.folder
 
-
         val folder = NovelsFragmentArgs.fromBundle(requireArguments()).folder
-//        val webNovelsList = folder.webNovels
+        webNovelsList = folder.webNovels
 
         //hardcoded for testing
-        val webNovelsList = getData()
+//        webNovelsList = getData()
 
-        Log.d("my_tag", "opened folder with name: ${folder.name} and ${webNovelsList.size} novels")
+        Log.d(TAG, "opened folder with name: ${folder.name} and ${webNovelsList.size} novels")
 
-        //TODO populate RecyclerView
+        // populate RecyclerView
         val rclView = view.findViewById<RecyclerView>(R.id.recycler_view2)
 
         //click listener for RecyclerView items
@@ -41,7 +45,7 @@ class NovelsFragment : Fragment() {
         }
 
         //Setup and display the RecyclerView
-        val novelsAdapter = NovelsAdapter(webNovelsList, onClickListener)
+        novelsAdapter = NovelsAdapter(webNovelsList, onClickListener)
         rclView.adapter = novelsAdapter
         rclView.layoutManager = LinearLayoutManager(context)
 
@@ -66,5 +70,53 @@ class NovelsFragment : Fragment() {
 //        menu.clear()
         menuInflater.inflate(R.menu.menu_toolbar_novels, menu)
         return super.onCreateOptionsMenu(menu, menuInflater)
+    }
+
+    //does something when a menu item is selected
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.add_webNovel -> {
+                val builder = AlertDialog.Builder(context)
+                builder.setTitle("Add Web Novel")
+
+                val viewInflated: View = LayoutInflater.from(context)
+                    .inflate(R.layout.popup_web_novel, view as ViewGroup?, false)
+
+                // Set up the input
+                val webNovelTitle = viewInflated.findViewById(R.id.et_webNovel_name) as TextInputEditText
+                val webNovelUrl = viewInflated.findViewById(R.id.et_webNovel_url) as TextInputEditText
+
+                // Specify the type of input expected
+                builder.setView(viewInflated)
+
+                builder.setPositiveButton(android.R.string.ok,
+                    DialogInterface.OnClickListener { dialog, which ->
+                        dialog.dismiss()
+
+                        val title = webNovelTitle.text.toString()
+                        val url = webNovelUrl.text.toString()
+
+                        Log.d(TAG, "new web novel requested: $title")
+
+                        addNovel(title, url)
+
+                    })
+
+                builder.setNegativeButton(android.R.string.cancel,
+                    DialogInterface.OnClickListener { dialog, which -> dialog.cancel() })
+
+                builder.show()
+
+                return true
+            }
+        }
+
+        return false
+    }
+
+    fun addNovel(title: String, url: String) {
+        Log.d(TAG, "adding new webNovel $title with url: $url")
+        webNovelsList.add( WebNovel(title, url) )
+        novelsAdapter.notifyItemInserted(webNovelsList.size - 1)
     }
 }
